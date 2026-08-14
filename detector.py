418faba3fd5bdef4676222750336b681f21a87c1
matiasschidiak <matiasschidiak@gmail.com>
2026-08-13 23:12:04 -0300

    Creando clase Detector.

--- detector.py
+++ detector.py
@@ -1,3 +1,8 @@
 import cv2
 from ultralytics import YOLO
 
+class Detector:
+    def __init__(self, modelo, fuente_video, umbral_confianza):
+        self.modelo = YOLO(modelo)
+        self.fuente_video = fuente_video
+        self.umbral_confianza = umbral_confianza
